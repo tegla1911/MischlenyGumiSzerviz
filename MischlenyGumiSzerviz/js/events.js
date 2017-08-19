7c9133186d91771e2d5a7be654764b96ef19c4e9
DD <deakd9@gmail.com>
2017-08-19 12:24:22 +0200

    t

---
+++ MischlenyGumiSzerviz/js/events.js
@@ -0,0 +1,26 @@
+﻿$("document").ready(function () {
+
+    var lastScrollTop = 0;
+    $(window).scroll(function (event) {
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop) {
+            $("#topNavbar").hide(300);
+        } else {
+            $("#topNavbar").show(300);
+        }
+        lastScrollTop = st;
+    });
+    $("#linkToTop").click(function () {
+        zenscroll.toY(30);
+    });
+
+    $("#dTireRep").click(function () {
+        ShowServAbout("dTireRep",1)
+    });
+
+});
+
+function ShowServAbout(caller,num) {
+    $("#dServAbou1").toggle(300);
+    zenscroll.
+}
