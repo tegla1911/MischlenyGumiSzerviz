3ea560926169c45eb8e492164800a4afc968a70d
DD <deakd9@gmail.com>
2017-09-14 21:50:47 +0200

    50%

--- MischlenyGumiSzerviz/js/events.js
+++ MischlenyGumiSzerviz/js/events.js
@@ -1,20 +1,10 @@
 ﻿$("document").ready(function () {
 
-
-    var lastScrollTop = 0;
-    $(window).scroll(function (event) {
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop) {
-            $("#topNavbar").hide(300);
-        } else {
-            $("#topNavbar").show(300);
-        }
-        lastScrollTop = st;
-    });
+    //ZenScroll
     $("#linkToTop").click(function () {
         zenscroll.toY(30);
     });
-
+    //Expanding divs
     $("#dTireRep, #dPunctureRep, #dBalancing").click(function () {
         ShowServAbout(1)
     });
@@ -22,16 +12,44 @@
         ShowServAbout(2)
     });
 
+    //carousel slick
+
+    //$('#carousel-images').slick({
+    //    arrows: true,
+    //    autoplay: true,
+    //    autoplaySpeed: 1000,
+    //});
 });
 
+
+//FUNCTIONS
 function ShowServAbout(num) {
     var aboutSelector = "#dServAbou" + num;
     $(aboutSelector).toggle(300);
 
 
-    if ($(aboutSelector).css('display') == 'none') {
+    if ($(aboutSelector).css('display') === 'none') {
         scrollTop: $(aboutSelector).offset().top
     }
 
 
 }
+
+
+
+
+
+//DEPRECATED
+
+//dissapperaing nav - used in document ready
+
+    // var lastScrollTop = 0;
+    //$(window).scroll(function (event) {
+    //    var st = $(this).scrollTop();
+    //    if (st > 30) {
+    //        $("#topNavbar").css("height",30);
+    //    } else {
+    //        $("#topNavbar").css("height", 30);
+    //    }
+    //    //lastScrollTop = st;
+    //});
